0a5306639618b9d48f2c76027086edd950e9fc4a
Shahzaib <smsashafqat@gmail.com>
2023-12-20 02:41:36 +0500

    updated ALLOWED_HOSTS

--- instagram_marketing/settings.py
+++ instagram_marketing/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-54kdmk+oih$=(indos_xtg+(^^8lj+dtwr(yjru4(*pyoqisgs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
