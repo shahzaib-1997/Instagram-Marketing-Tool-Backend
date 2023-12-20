9608a6c3c326867ec5b0c9b6f3d08ef89e80bc15
Shahzaib <smsashafqat@gmail.com>
2023-12-21 02:01:19 +0500

    added ready method to autorun on startup

--- userapi/apps.py
+++ userapi/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class UserapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userapi'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "userapi"
+
+    def ready(self):
+        # Your startup code here
+        print("Startup code executed")
