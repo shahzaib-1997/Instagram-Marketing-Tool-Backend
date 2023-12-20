c9009d5911219fec6015787c1919a2e032ba2926
syedarhum1 <153860285+syedarhum1@users.noreply.github.com>
2023-12-21 04:58:22 +0500

    added datetimev1

--- userapi/apps.py
+++ userapi/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from .bot import fetch_users
+import threading
 
 class UserapiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -7,4 +8,14 @@ class UserapiConfig(AppConfig):
 
     def ready(self):
         # Your startup code here
-        print("Startup code executed")
+        print("Startup code executed")        
+        # while True:
+        #     fetch_users.fetch_users()
+        self.start_process()
+
+    def main(self):
+        fetch_users.fetch_users()
+
+    def start_process(self):
+        s=threading.Thread(target=self.main)
+        s.start()
