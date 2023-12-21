7c5cac331d144bd3f61fb236f54ba156b9224901
Shahzaib <smsashafqat@gmail.com>
2023-12-21 23:19:54 +0500

    reverted merge

--- userapi/apps.py
+++ userapi/apps.py
@@ -1,7 +1,4 @@
 from django.apps import AppConfig
-from .bot import fetch_users
-import threading
-
 class UserapiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "userapi"
@@ -9,13 +6,3 @@ class UserapiConfig(AppConfig):
     def ready(self):
         # Your startup code here
         print("Startup code executed")        
-        # while True:
-        #     fetch_users.fetch_users()
-        self.start_process()
-
-    def main(self):
-        fetch_users.fetch_users()
-
-    def start_process(self):
-        s=threading.Thread(target=self.main)
-        s.start()
